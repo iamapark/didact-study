c6d36042e3091f26a83768ab37450f217f9b55b8
Jinyoung Park <iamapark89@gmail.com>
2026-08-09 21:04:44 +0900

    step 6

--- src/main.jsx
+++ src/main.jsx
@@ -38,10 +38,66 @@ function createDom(fiber) {
   return dom
 }
 
+const isEvent = key => key.startsWith("on")
+const isProperty = key => 
+  key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      key => 
+        !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    ).forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.removeEventListener(
+        eventType,
+        prevProps[name]
+      )
+    })
+  
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    })
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.addEventListener(
+        eventType,
+        nextProps[name]
+      )
+    })
+}
+
 // Fiber tree를 돌면서 생성만 해놓은 Dom 객체를 Document에 한꺼번에 연결
 // 이 사이에 브라우저 페인트가 끼어들 수 없음
 function commitRoot() {
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
@@ -50,7 +106,23 @@ function commitWork(fiber) {
     return
   }
   const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
+  if (
+    fiber.effectTag === 'PLACEMENT' &&
+    fiber.dom != null
+  ) {
+    domParent.appendChild(fiber.dom)
+  } else if (
+    fiber.effectTag === 'UPDATE' &&
+    fiber.dom != null
+  ) {
+    updateDom(
+      fiber.dom,
+      fiber.alternate.props,
+      fiber.props
+    )
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom)
+  }
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
@@ -60,13 +132,17 @@ function render(element, container) {
     dom: container,
     props: {
       children: [element],
-    }
+    },
+    alternate: currentRoot, // 이전 커밋 단계에서 DOM에 커밋했던 기존 fiber에 대한 링크
   }
+  deletions = []
   nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let currentRoot = null  // DOM에 커밋한 마지막 fiber tree에 대한 참조
 let wipRoot = null
+let deletions = null
 
 function workLoop(deadline) {
   let shouldYield = false
@@ -90,40 +166,14 @@ requestIdleCallback(workLoop)
 /**
  * 각 fiber에는 첫번째 child, 다음 sibling, 그리고 parent로 가는 링크가 있음
  */
-
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
   }
 
-  // if (fiber.parent) {
-  //   fiber.parent.dom.appendChild(fiber.dom)
-  // }
-
   const elements = fiber.props.children
-  let index = 0
-  let prevSibling = null
-
-  while (index < elements.length) {
-    const element = elements[index]
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    }
-
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {
-      prevSibling.sibling = newFiber
-    }
-
-    prevSibling = newFiber
-    index++
-  }
-
+  reconcileChildren(fiber, elements)
+  
   if (fiber.child) {    // 규칙 1: 자식이 있으면 자식으로
     return fiber.child
   }
@@ -135,6 +185,67 @@ function performUnitOfWork(fiber) {
     nextFiber = nextFiber.parent  // 규칙 3: 없으면 한 칸 올라가서 다시 형제 찾기
   }
   // root까지 올라와도 형제가 없으면 while 종료 → undefined 반환
+}
+
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = 
+    wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+
+  while (
+    index < elements.length ||
+    oldFiber != null
+  ) {
+    const element = elements[index]
+    let newFiber = null
+
+    const sameType = 
+      oldFiber &&
+      element &&
+      element.type === oldFiber.type
+    
+    if (sameType) {
+      // UPDATE
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE"
+      }
+    }
+    if (element && !sameType) {
+      // PLACEMENT
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT"
+      }
+    }
+    if (oldFiber && !sameType) {
+      // DELETE
+      oldFiber.effectTag = "DELETION"
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else if(element) {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
 }
 
 const Didact = {
